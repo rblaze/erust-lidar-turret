dc903ab3051b02412f2b98fbe5b16d775323a451
Andrey Sverdlichenko <blaze@rusty.zone>
2026-06-08 23:09:35 -0400

    Basic LIDAR support

--- cross/app/src/main.rs
+++ cross/app/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![deny(unsafe_code)]
 
-use core::cell::RefCell;
+use core::cell::{Cell, RefCell};
 use core::panic::PanicInfo;
 
 use cortex_m::asm::delay;
@@ -12,11 +12,13 @@ use embedded_hal::pwm::SetDutyCycle;
 use rtt_target::debug_rprintln;
 #[cfg(debug_assertions)]
 use rtt_target::rtt_init_print;
-use stm32g0_hal::gpio::gpioa::PA0;
+use stm32g0_hal::gpio::gpioa::{PA0, PA2, PA3};
 use stm32g0_hal::gpio::{Alternate, GpioExt};
-use stm32g0_hal::pac::{CorePeripherals, Interrupt, NVIC, Peripherals, TIM2, interrupt};
+use stm32g0_hal::pac::{
+    CorePeripherals, DMA1, Interrupt, NVIC, Peripherals, TIM2, USART2, interrupt,
+};
 use stm32g0_hal::rcc::config::{Config, Prescaler};
-use stm32g0_hal::rcc::{HSI_FREQ, RccExt, ResetEnable};
+use stm32g0_hal::rcc::{RccExt, ResetEnable};
 use stm32g0_hal::timer::TimerExt;
 
 use firmware::error::Error;
@@ -123,34 +125,124 @@ fn main() -> ! {
 
         speed_timer.cr1().write(|w| w.cen().enabled());
 
+        // USART RX DMA
+        let _tx: PA2<Alternate<1>> = gpioa.pa2.into_alternate_function();
+        let _rx: PA3<Alternate<1>> = gpioa.pa3.into_alternate_function();
+        let usart = dp.USART2;
+
+        let rx_dma = dp.DMA1.ch1();
+        DMA1::enable(&rcc);
+        DMA1::reset(&rcc);
+
+        #[allow(unsafe_code)]
+        unsafe {
+            rx_dma
+                .par()
+                .write(|w| w.pa().bits(usart.rdr() as *const _ as u32));
+            critical_section::with(|cs| {
+                let ptr = LIDAR_DMA_BUFFER.borrow(cs).as_ptr();
+                rx_dma.mar().write(|w| w.ma().bits(ptr as u32));
+            });
+        }
+        rx_dma
+            .ndtr()
+            .write(|w| w.ndt().set(LIDAR_BUFFER_SIZE as u16));
+        rx_dma.cr().write(|w| {
+            w.mem2mem()
+                .disabled()
+                .msize()
+                .bits8()
+                .minc()
+                .enabled()
+                .psize()
+                .bits32()
+                .pinc()
+                .disabled()
+                .circ()
+                .enabled()
+                .dir()
+                .from_peripheral()
+                .htie()
+                .enabled()
+                .tcie()
+                .enabled()
+        });
+
+        let dmamux = dp.DMAMUX;
+        #[allow(unsafe_code)]
+        unsafe {
+            // USART2 DMA request id is 52
+            dmamux.ccr(0).write(|w| w.dmareq_id().bits(52));
+        }
+        // Enable DMA channel
+        rx_dma.cr().modify(|_, w| w.en().enabled());
+
+        // USART for communicating with LIDAR
+        USART2::enable(&rcc);
+        USART2::reset(&rcc);
+
+        // Set 8 bit word length, no parity
+        usart
+            .cr1()
+            .write(|w| w.m1().clear_bit().m0().clear_bit().pce().disabled());
+        // Set 1 stop bit
+        usart.cr2().write(|w| w.stop().stop1());
+        // Set bit rate to 115200. Assume usart_presc=1, USART running at MCU clock.
+        let usart_div = rcc.sysclk().to_Hz() / 115200;
+        usart.brr().write(|w| w.brr().set(usart_div as u16));
+        // Enable USART
+        usart.cr1().modify(|_, w| w.ue().enabled());
+        // Enable RX DMA
+        usart.cr3().write(|w| w.dmar().enabled());
+        // Enable transmitter and receiver.
+        usart.cr1().modify(|_, w| w.te().enabled().re().enabled());
+
+        // Enable interrupts
         #[allow(unsafe_code)]
         unsafe {
             NVIC::unmask(Interrupt::TIM2);
+            NVIC::unmask(Interrupt::DMA1_CHANNEL1);
         }
 
-        debug_rprintln!("sleeping");
-        delay(HSI_FREQ.to_Hz());
+        // debug_rprintln!("sleeping");
+        // delay(rcc.sysclk().to_Hz());
         debug_rprintln!("entering control loop");
 
+        // for ch in [0x5a, 0x04, 0x01, 0x00] {
+        //     while usart.isr().read().txe().bit_is_clear() {}
+        //     debug_rprintln!("sending {:02x}", ch);
+        //     usart.tdr().write(|w| w.tdr().set(ch));
+        // }
+
+        // while usart.isr().read().tc().bit_is_clear() {}
+
+        // debug_rprintln!("done sending");
+
         loop {
-            for _ in 0..NUM_SPEED_POINTS {
-                cortex_m::asm::wfi();
-            }
-
-            const NUM_WHEEL_SLOTS: u32 = 25;
-
-            let speed_data = critical_section::with(|cs| SPEED_TICKS.borrow_ref(cs).speed_data);
-            let average_speed = speed_data.iter().sum::<u32>() / (NUM_SPEED_POINTS as u32);
-            let freq = HSI_FREQ.to_Hz() / average_speed;
-
-            // This calculates new duty cycle
-            let error = freq as f32 / NUM_WHEEL_SLOTS as f32;
-            duty = (duty as f32 / error) as u16;
-
-            motor_pin.set_duty_cycle(duty);
-
-            debug_rprintln!("freq: {}, error: {}, new duty: {}", freq, error, duty);
+            cortex_m::asm::wfi();
+            // let frame = critical_section::with(|cs| LAST_FRAME.borrow(cs).get());
+            // debug_rprintln!("frame {:x?}", frame.data);
         }
+
+        // loop {
+        // for _ in 0..NUM_SPEED_POINTS {
+        // cortex_m::asm::wfi();
+        // }
+
+        // const NUM_WHEEL_SLOTS: u32 = 25;
+
+        // let speed_data = critical_section::with(|cs| SPEED_TICKS.borrow_ref(cs).speed_data);
+        // let average_speed = speed_data.iter().sum::<u32>() / (NUM_SPEED_POINTS as u32);
+        // let freq = HSI_FREQ.to_Hz() / average_speed;
+
+        // // This calculates new duty cycle
+        // let error = freq as f32 / NUM_WHEEL_SLOTS as f32;
+        // duty = (duty as f32 / error) as u16;
+
+        // motor_pin.set_duty_cycle(duty);
+
+        // debug_rprintln!("freq: {}, error: {}, new duty: {}", freq, error, duty);
+        // }
     }()
     .expect("error in main");
 
@@ -182,4 +274,26 @@ fn TIM2() {
         info.speed_data[index] = ticks;
         info.next_index = (index + 1) % NUM_SPEED_POINTS;
     });
+}
+
+const LIDAR_BUFFER_SIZE: usize = 1024;
+static LIDAR_DMA_BUFFER: Mutex<Cell<[u8; LIDAR_BUFFER_SIZE]>> =
+    Mutex::new(Cell::new([0; LIDAR_BUFFER_SIZE]));
+
+#[interrupt]
+fn DMA1_CHANNEL1() {
+    #[allow(unsafe_code)]
+    let rx_dma = unsafe { DMA1::steal() };
+
+    if rx_dma.isr().read().htif1().is_half() {
+        debug_rprintln!("half buffer");
+        // TODO: copy half data
+        rx_dma.ifcr().write(|w| w.chtif1().clear());
+    }
+
+    if rx_dma.isr().read().tcif1().is_complete() {
+        debug_rprintln!("full buffer");
+        // TODO: copy second half data
+        rx_dma.ifcr().write(|w| w.ctcif1().clear());
+    }
 }
